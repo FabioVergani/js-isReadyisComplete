0d910671ee2a8a8050c51ac1277d877e4a937108
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-06-30 13:02:56 +0200

    Update blueprint2.js

--- blueprint2.js
+++ blueprint2.js
@@ -1,26 +1,25 @@
 (function($w){'use strict';
 
  function isReady(evt){
-	var isEvt=true, e=evt, d=e.target||(isEvt=false,e), dECL=d.documentElement.classList;
-	d.title=d.titleOriginal;
-	if(isEvt){dECL.add('ready');};
+	var k=true,e=evt,d=e.target||(k=false,e),w=d.defaultView;
 	w.scrollTo(0,0);
+	if(w.support.DOMContentLoaded=k){d.documentElement.classList.add('ready');};
+	d.title=d.titleOriginal;
  }
- //
-
 
  function isLoad(evt){
-	var isEvt=true, e=evt, d=e.target||(isEvt=false,e), dECL=d.documentElement.classList;
+	var e=evt,d=e.target||e,w=d.defaultView,o=d.documentElement.classList;
+	if(w.support.DOMContentLoaded===true){o.remove('ready');}else{isReady(d);};
 	d.title=d.titleOriginal;
-	if(isEvt||dECL.contains('ready')){dECL.remove('ready');};
-	dECL.add('loaded');
-	w.scrollTo(0,1);//hide Old iOS AddressBar?
+	o.add('loaded');
+	w.setTimeout(function(){w.scrollTo(0,1);},9);//hide Old iOS AddressBar?
  }
- //
+
 
  var w=$w,
  d=(function($w){
 	var i,w=$w,d=w.document;
+	w.support={DOMContentLoaded:false};
 	d.titleOriginal=d.title;
 	d.title='Loading..';
 	if(i=d.documentMode && i<9){i=3;for(var m=['footer','header','main','nav'];i--;d.createElement(m[i--]));};
@@ -32,6 +31,12 @@
  stopListen=w.removeEventListener,
  at=function(s,f,b){var t=s,i=function(o){f(o);stopListen(t,i);};on(t,i,b&&true);};
  //
- if(d.readyState!=='complete'){at('DOMContentLoaded',isReady);at('load',isLoad);}else{isReady(d);isLoad(d);};
+ if(d.readyState!=='complete'){
+	at('DOMContentLoaded',isReady);
+	at('load',isLoad);
+ }else{
+	//isReady(d);
+	isLoad(d);
+ };
 })(window);
 //END.
