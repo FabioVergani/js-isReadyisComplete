4ae5f670f7f62a86e45786a7c8e5607a0186bd2e
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-04-02 20:10:57 +0200

    Update wellformed.js

--- 4/wellformed.js
+++ 4/wellformed.js
@@ -1,16 +1,16 @@
 (function(w){'use strict';
- function at(e,s,f,x){function a(){e.removeEventListener(s,i)}function i(o){a();f(o)}a();e.addEventListener(s,i,x)};
+ function at(e,s,f,x){function a(){e.removeEventListener(s,i)}function i(o){a();f(o)}a();e.addEventListener(s,i,x)}
  function swapClass(e,a,b){var o=e.classList;o.remove(a);o.add(b);return o;}
- function a(evt){
+ function a(evt){//@Ready
   const d=evt.target, w=d.defaultView, dE=d.documentElement;
 	//‹‹‹
-
+ 	
 	console.log('');
 
 	//‹‹‹
-	swapClass(dE,'loaded','loading').add('ready')//@Ready:done
+	swapClass(dE,'loading','ready')
  }
- function b(evt){
+ function b(evt){//@Load
 	const d=evt.target, w=d.defaultView, dE=d.documentElement;
 	let s;
 	//‹‹‹
@@ -18,18 +18,19 @@
 	console.log('');
 
 	//‹‹‹
-	swapClass(dE,'ready','loaded').remove('loading');
-	if(s=d.titleOriginal){d.title=s}//@Load:done
+	swapClass(dE,'ready','loaded');
+	if(s=d.titleOriginal){d.title=s}
  }
- let e=w.document;
- if(e.readyState!=='complete'){
-	e.titleOriginal=e.title;
-	e.title='Loading..';
+ let d=w.document;
+ if(d.readyState!=='complete'){
+	swapClass(d.documentElement,'loaded','loading');
+	d.titleOriginal=d.title;
+	d.title='Loading..';
 	at(w,'DOMContentLoaded',a);
 	at(w,'load',b)
  }else{
-	a(e={target:e});
-	b(e)
+	a(d={target:d});
+	b(d)
  };
 })(window);
 //END.
