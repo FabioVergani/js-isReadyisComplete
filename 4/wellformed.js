67f3aa91d898b84b4c485ff741ea88c5217b746c
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-04-02 19:28:21 +0200

    Update wellformed.js

--- 4/wellformed.js
+++ 4/wellformed.js
@@ -2,22 +2,26 @@
  function at(e,s,f,x){function a(){e.removeEventListener(s,i)}function i(o){a();f(o)}a();e.addEventListener(s,i,x)};
  function swapClass(e,a,b){var o=e.classList;o.remove(a);o.add(b);return o;}
  function a(evt){
+  const d=evt.target, w=d.defaultView, dE=d.documentElement;
 	//‹‹‹
+
+	console.log('');
 
 	//‹‹‹
 	swapClass(dE,'loaded','loading').add('ready')//@Ready:done
  }
  function b(evt){
+	const d=evt.target, w=d.defaultView, dE=d.documentElement;
 	let s;
-	const d=evt.target,dE=d.documentElement;
 	//‹‹‹
+
+	console.log('');
 
 	//‹‹‹
 	swapClass(dE,'ready','loaded').remove('loading');
 	if(s=d.titleOriginal){d.title=s}//@Load:done
  }
  let e=w.document;
- const $id=e.getElementById.bind(e);
  if(e.readyState!=='complete'){
 	e.titleOriginal=e.title;
 	e.title='Loading..';
