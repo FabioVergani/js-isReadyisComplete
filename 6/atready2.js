8eef71f6b79a33c142cfcbfea89e7f09a22f2dc2
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-06-19 18:37:21 +0200

    Update atready2.js

--- 6/atready2.js
+++ 6/atready2.js
@@ -1,1 +1,19 @@
+(function($){
+ function isReady(event_Ready){console.info('isReady%O',event_Ready);
+  const eR=event_Ready, $d=eR.target;
+	return eR;
+ }
+ function isLoad(event_Load){console.info('isLoad%O',event_Load);
+  const $d=event_Load.target;
 
+	//#
+  listen('keyup',function(event_KeyUp){
+ 		const n=event_KeyUp.keyCode;console.info('KeyUp%O',event_KeyUp,n);
+  });
+
+ }
+ function listen(a,b){const w=$;w.removeEventListener(a,b);w.addEventListener(a,b)}
+ function at(a,b){const w=$,c=function(x){b(x);w.removeEventListener(a,c)};listen(a,c)}
+ const $d=$.document;
+ if($d.readyState!=='complete'){at('DOMContentLoaded',isReady);at('load',isLoad)}else{isLoad(isReady({target:$d}))}
+})(window);
