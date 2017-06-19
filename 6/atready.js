24dca6622902fae84ced9dce75b08a883c7a5f77
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-06-19 12:48:26 +0200

    Update atready.js

--- 6/atready.js
+++ 6/atready.js
@@ -1,11 +1,10 @@
 (function($){
- function isReady(event_Ready){console.info('isReady%O',event_Ready);
-  const $d=event_Ready.target;
-	return $d
+ function isReady(event_Ready){//console.info('isReady%O',event_Ready);
+  const eR=event_Ready, $d=eR.target;
+	return eR;
  }
- function isLoad(event_Load){console.info('isLoad%O',event_Load);
+ function isLoad(event_Load){//console.info('isLoad%O',event_Load);
   const $d=event_Load.target;
-	return $d
  }
  function at(a,b){const c=function(x){b(x);w.removeEventListener(a,c)},w=$;w.addEventListener(a,c)}
  const $d=$.document;
