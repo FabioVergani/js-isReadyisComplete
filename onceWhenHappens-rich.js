da23333a9289466a2634e1a3838c708f0ab5e8c9
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-11-14 14:43:12 +0100

    Update onceWhenHappens-rich.js

--- onceWhenHappens-rich.js
+++ onceWhenHappens-rich.js
@@ -1,30 +1,48 @@
 (w=>{
-	const c=w.console,d=w.document,onceWhenHappens=(e,s,c)=>{const x=o=>{e.removeEventListener(s,x);c(o)};e.addEventListener(s,x)},
+	const c=w.console,
+	d=w.document,
+	frag=w.Object.defineProperty(d.createDocumentFragment(),'createElement',{value:s=>frag.appendChild(d.createElement(s))}),
+	onceWhenHappens=(e,s,c)=>{const x=o=>{e.removeEventListener(s,x);c(o)};e.addEventListener(s,x)},
 	domReady=domIsReady=>{
 		c.log('dom is: ready %O',domIsReady);
 		const d=domIsReady.target,
-		frag=w.Object.defineProperty(d.createDocumentFragment(),'add',{value:s=>frag.appendChild(d.createElement(s))}),
-		$body=d.body,
-		$header=frag.add('header'),
-		$footer=frag.add('footer'),
-		$main=d.getElementById('main');
+		$header=frag.createElement('header'),
+		$footer=frag.createElement('footer'),
+		$body=d.body,	
+		$contents=d.getElementById('contents');
 		//
-		$header.id='header';
-		$footer.id='footer';
+		//...
 		//
-		(e=>{
-			const s=e.style,v=s.display;
-			s.display='none';
-			e.insertBefore($header,e.firstElementChild);
-			e.appendChild($footer);
+		(b=>{
+			const s=b.style,v=s.display;
+			let e=$header;
+			e.id='header';
+			b.display='none';
+			b.classList.remove('js-no');
+			b.insertBefore(e,b.firstElementChild);
+			e=$footer;
+			e.id='footer';
+			b.appendChild(e);
 			s.display=v
 		})($body);
-		//
-		if(domIsReady.type===null||d.readyState==='complete'){docLoaded({target:d,type:null})}else{onceWhenHappens(w,'load',docLoaded)}
+		//--------------------------------------------------------------
+		if(domIsReady.type===null||d.readyState==='complete'){
+			docLoaded({target:d,type:null})
+		}else{
+			$body.classList.add('loading');
+			onceWhenHappens(w,'load',docLoaded)
+		}
 	},
 	docLoaded=docIsLoaded=>{
 		c.log('document is: loaded %O',docIsLoaded);
-		//... faicose con il doc...
+		const d=docIsLoaded.target,
+		$body=d.body,
+		$bodyClassList=$body.classList;
+		if(docIsLoaded.type!==null){
+			$bodyClassList.replace('loading','loaded');
+		}else{
+			$bodyClassList.add('loaded');
+		}
 	};
 	//
 	if(d.readyState!=='complete'){onceWhenHappens(w,'DOMContentLoaded',domReady)}else{domReady({target:d,type:null})}
