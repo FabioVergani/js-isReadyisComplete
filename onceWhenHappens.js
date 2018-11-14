87491f60a7fe54d706b440e19c53079140d9e67d
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-11-14 11:54:50 +0100

    Update onceWhenHappens.js

--- onceWhenHappens.js
+++ onceWhenHappens.js
@@ -2,7 +2,7 @@
 	const c=w.console,d=w.document,onceWhenHappens=(e,s,c)=>{const x=o=>{e.removeEventListener(s,x);c(o)};e.addEventListener(s,x)},
 	domReady=domIsReady=>{
 		c.log('dom is: ready %O',domIsReady);
-		const d=event.target,$body=d.body;
+		const d=domIsReady.target,$body=d.body;
 		//... faicose con il dom ...
 		if(domIsReady.type===null||d.readyState==='complete'){docLoaded({target:d,type:null})}else{onceWhenHappens(w,'load',docLoaded)}
 	},
