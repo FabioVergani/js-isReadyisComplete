319eaf17ff1dbe3a62ec8c857f9d70126da4a2c0
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-05-18 20:03:34 +0200

    Update onceReady.js

--- onceReady/onceReady.js
+++ onceReady/onceReady.js
@@ -5,17 +5,18 @@ const onceAt=(e,s,f)=>{
 		f(o)
 	};
 	e.addEventListener(s,x)
-},
-onceReady=(w,f)=>{
+};
+
+const onceReady=(w,f)=>{
 	if('complete'!==w.document.readyState){
 		onceAt(w,'load',f)
 	}else{
 		f(w)
 	}
 };
-
 //...
-
-onceReady(window,w=>{
+onceReady(window,o=>{
+console.dir(o)
 	alert('ready')
-});
+})
+
